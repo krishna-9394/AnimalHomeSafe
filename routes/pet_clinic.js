a32e8ed9ecfe28b504cf0f12060e412ea37974a4
Krishna <krishnakumar72480@gmail.com>
2023-10-10 07:34:21 +0530

    completed with the endPoints of pet_clinic such as list, id, city, and state

--- routes/pet_clinic.js
+++ routes/pet_clinic.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router();
+const connection = require('../db');
 
 router.get('/', (req, res) => {
     res.send('List of all pet clinic');
@@ -7,17 +8,43 @@ router.get('/', (req, res) => {
 
 router.get('/id', (req, res) => {
     const userId = req.query.id;
-    res.send('Details of Pet_clinic with Id: '+userId);
+    const sql_query = "select * from address where address_id = "+userId+";";
+    connection.query(sql_query, (err, results) => {
+        if (err) {
+            console.error('Error fetching data:', err);
+            res.send('Error fetching data from database');
+            return;
+        }
+        res.json(results);
+    });
 });
 
 router.get('/city', (req,res) => {
     const city_name = req.query.city;
-    res.send('List of Pet_clinic in City: '+city_name);
+    const sql_query = 'select * from address where city = "' + city_name + '";';
+    connection.query(sql_query, (err, results) => {
+        if (err) {
+            console.error('Error fetching data:', err);
+            res.send('Error fetching data from database');
+            return;
+        }
+        console.log(results);
+        res.json(results);
+    });
 });
 
 router.get('/state', (req, res) => {
-    const state = req.query.state;
-    res.send('List of Pet_clinic in state: '+state);
+    const state_name = req.query.state;
+    const sql_query = 'select * from address where city = "' + state_name + '";';
+    connection.query(sql_query, (err, results) => {
+        if (err) {
+            console.error('Error fetching data:', err);
+            res.send('Error fetching data from database');
+            return;
+        }
+        console.log(results);
+        res.json(results);
+    });
 });
 
 
