6633f0d99e98280fd7e68b2de6e6fb4b00b22e3c
Krishna <krishnakumar72480@gmail.com>
2023-10-18 19:00:23 +0530

    treid connecting the stores with product name in findNearest RetailStore

--- routes/retail_store.js
+++ routes/retail_store.js
@@ -7,7 +7,7 @@ const connection = require('../db');
 
 const baseQuery = "select rs.shop_id as id, rs.shop_name as shop_name, rs.owner_name as owner_name, rs.contact_number as contact_number, adr.street_name as street_name, adr.city as city, adr.state as state, adr.pincode from retail_store as rs join address as adr on adr.address_id = rs.address_id";
 
-const customQuery = "select rs.shop_id as id, rs.shop_name as shop_name, rs.owner_name as owner_name, rs.contact_number as contact_number from retail_store as rs join address as adr on adr.address_id = rs.address_id";
+const customQuery = "select rs.shop_id as id, rs.shop_name as shop_name, rs.owner_name as owner_name, rs.contact_number as contact_number, adr.street_name as street_name, adr.city as city, adr.state as state, adr.pincode from retail_store as rs join address as adr join products_list_of_all_shops as list on adr.address_id = rs.address_id and rs.shop_id = list.shop_id where list.product";
 
 
 router.get('/', (req, res) => {
@@ -36,7 +36,7 @@ router.get('/', (req, res) => {
 
 router.get('/id', (req, res) => {
     const userId = req.query.id;
-    const sql_query = baseQuery+" and rs.place_id = " + userId + ";";
+    const sql_query = baseQuery+" and rs.shop_id = " + userId + ";";
     connection.query(sql_query, (err, results) => {
         if (err) {
             console.error('Error fetching data:', err);
@@ -130,15 +130,31 @@ router.get('/store_name', (req, res) => {
 
 router.get('/product_name',(req,res) =>{
     const productName = req.query.product_name;
-    const sql_query = baseQuery+" where rs.shop_name LIKE '" + storeName + "%' OR rs.shop_name LIKE '%"+storeName+"' OR rs.shop_name LIKE '%"+storeName+"%';";
+    const sql_query = "select product_id as id from product where product_name LIKE '" + productName + "%' OR product_name LIKE '%"+productName+"' OR product_name LIKE '%"+productName+"%';";
     connection.query(sql_query, (err, results) => {
         if (err) {
             console.error('Error fetching data:', err);
             res.send('Error fetching data from database');
             return;
         }
+        var data = [];
         console.log(results);
-        const modifiedData = results.map(result => ({
+         const promises = results.map(result => {
+            return new Promise((resolve, reject) => {
+                const sql_query = baseQuery + " and rs.shop_id = " + (result.id) + ";";
+                connection.query(sql_query, (err, storeResults) => {
+                    if (err) {
+                        console.error('Error fetching data:', err);
+                        reject(err);
+                        return;
+                    }
+                    storeResults.forEach(item => data.push(item));
+                    resolve();
+                });
+            });
+        });
+        console.log(data);
+         const modifiedData = data.map(result => ({
             storeId: result.id.toString(), // Convert the id to a string format
             storeName: result.shop_name,
             ownerName: result.owner_name,
@@ -151,8 +167,6 @@ router.get('/product_name',(req,res) =>{
         res.json(modifiedData);
     });
 })
-
-// TODO 1: Add endpoint to list of items available in a shop
 
 router.get('/id/products',(req,res) => {
     const shop_id = req.query.shop_id;
@@ -169,19 +183,19 @@ router.get('/id/products',(req,res) => {
 
 // TODO 2: Add endpoint to get details of a certain product
 
-router.get('/product/id',(req,res) => {
-    const product_id = req.query.product_id;
-    const sql_query = 'select p.product_id as id, p.product_name as name, p.product_discription as discription, p.cost as cost from product where product_id = '+product_id+';';
-    connection.query(sql_query, (err, results) => {
-        if (err) {
-            console.error('Error fetching data:', err);
-            res.send('Error fetching data from database');
-            return;
-        }
-        console.log(results);
-        res.json(results);
-    });
-})
+// router.get('/product/id',(req,res) => {
+//     const product_id = req.query.product_id;
+//     const sql_query = 'c
+//     connection.query(sql_query, (err, results) => {
+//         if (err) {
+//             console.error('Error fetching data:', err);
+//             res.send('Error fetching data from database');
+//             return;
+//         }
+//         console.log(results);
+//         res.json(results);
+//     });
+// })
 
 module.exports = router;
 
