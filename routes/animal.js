2d9e67c64ad870ad7b3aa8192f3aa6d7bb34f747
Ekank Chhaparwal <ekank1410@gmail.com>
2023-10-18 22:41:27 +0530

    Completed Stray Animals Management

--- routes/animal.js
+++ routes/animal.js
@@ -12,4 +12,18 @@ router.get('/id', (req,res) => {
     // details of specific details
 });
 
-module.export = router;
+const base_Query2 = 'SELECT Guidelines FROM StrayAnimalGuidelines'
+router.get('/strayAnimals', (req, res) => {
+    const animal_name = req.query.strayAnimals; 
+    const sql_query = base_Query2 + " where AnimalType = '" + animal_name+ "'"; 
+    connection.query(sql_query, (err, results) => {
+        if (err) {
+            console.error('Error fetching data:', err);
+            res.send('Error fetching data from database');
+            return;
+        }
+        res.json(results);
+    });
+});
+
+module.exports = router;
