966f89c614ac07b72f7c6ccfbd2c92ba692a109f
Ekank Chhaparwal <ekank1410@gmail.com>
2023-10-21 10:14:12 +0530

    Added Procedures in mysql for signUp

--- frontend/gentleGuardians.sql
+++ frontend/gentleGuardians.sql
@@ -68,7 +68,7 @@ CREATE TABLE PRODUCTS_LIST_OF_ALL_SHOPS (
   PRIMARY KEY (shop_id, product_id)
 );
 
-
+-- drop table ANIMALS_LIST_IN_ALL_PLACES;
 
 
 
@@ -339,6 +339,7 @@ VALUES
 ('jyoti@email.com', 'hashed_password19'),
 ('karan@email.com', 'hashed_password20');
 
+
 CREATE TABLE ANIMAL (
     animal_id INT PRIMARY KEY,
     description_info TEXT,
@@ -351,8 +352,10 @@ CREATE TABLE ANIMAL (
     max_temp_of_survival DECIMAL(5,2),
     min_temp_of_survival DECIMAL(5,2),
     isDomestic BOOLEAN,
-    common_animal_name VARCHAR(255)
+    common_animal_name VARCHAR(255),
+    imageURL VARCHAR(1000)
 );
+
 INSERT INTO ANIMAL 
 (animal_id, description_info, animal_name, breed, allergens, size, diet_plan, gender, max_temp_of_survival, min_temp_of_survival, isDomestic, common_animal_name, imageURL) 
 VALUES 
@@ -361,7 +364,7 @@ VALUES
 (33, 'Robust and strong.', 'Buffalo', 'Water Buffalo', 'Dander', 'Large', 'Grass, aquatic plants.', 'Male', 33, -10, FALSE, 'Buffalo', 'https://unsplash.com/photos/thC1uwWdMfM'),
 (34, 'Calm and sociable.', 'Cow', 'Angus', 'Dander', 'Large', 'Grass, hay.', 'Female', 33, -15, TRUE, 'Cow', 'https://images.unsplash.com/photo-1570042225831-d98fa7577f1e?auto=format&fit=crop&q=60&w=600&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8Y293fGVufDB8fDB8fHww'),
 (35, 'Docile and woolly.', 'Sheep', 'Merino', 'Dander', 'Medium', 'Grass, hay.', 'Male', 30, -10, TRUE, 'Sheep', 'https://images.unsplash.com/photo-1588942173353-0c53a1bf9081?auto=format&fit=crop&q=60&w=600&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8c2hlZXB8ZW58MHx8MHx8fDA%3D'),
-(36, 'Majestic and regal.', 'Eagle', 'Bald Eagle', 'Feather dust', 'Medium', 'Fish, small mammals.', 'Male', 40, 5, FALSE, 'Bird' 'https://plus.unsplash.com/premium_photo-1674458852390-ac22d6c502d7?auto=format&fit=crop&q=80&w=2071&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),
+(36, 'Majestic and regal.', 'Eagle', 'Bald Eagle', 'Feather dust', 'Medium', 'Fish, small mammals.', 'Male', 40, 5, FALSE, 'Bird' ,'https://plus.unsplash.com/premium_photo-1674458852390-ac22d6c502d7?auto=format&fit=crop&q=80&w=2071&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),
 (37, 'Stealthy and fierce.', 'Leopard', 'African Leopard', 'Dander', 'Medium', 'Meat, small mammals.', 'Female', 38, -5, FALSE, 'Wild Cat', 'https://images.unsplash.com/photo-1544985361-b420d7a77043?auto=format&fit=crop&q=60&w=600&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bGVvcGFyZHxlbnwwfHwwfHx8MA%3D%3D'),
 (38, 'Elegant and swift.', 'Antelope', 'Springbok', 'None', 'Medium', 'Grass, shrubs.', 'Male', 35, -10, FALSE, 'Antelope', 'https://images.unsplash.com/photo-1566034356854-23137d8128de?auto=format&fit=crop&q=60&w=600&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YW50ZWxvcGV8ZW58MHx8MHx8fDA%3D'),
 (39, 'Sly and adaptable.', 'Fox', 'Red Fox', 'Dander', 'Small', 'Meat, insects, fruits.', 'Female', 33, -15, FALSE, 'Fox', 'https://images.unsplash.com/photo-1474511320723-9a56873867b5?auto=format&fit=crop&q=60&w=600&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Rm94fGVufDB8fDB8fHww'),
@@ -621,36 +624,6 @@ select * from StrayAnimalGuidelines;
 
 -- Creting adoption center table 
 
-CREATE TABLE ADOPTION_CENTER (
-  id INTEGER PRIMARY KEY,
-  name VARCHAR(50),
-  address_id INTEGER,
-  contact_number VARCHAR(15),
-  FOREIGN KEY (address_id) REFERENCES ADDRESS(address_id)
-);
-
--- Inserting Sample Data for Adoption Centers with Randomized Address IDs
-INSERT INTO ADOPTION_CENTER (id, name, address_id, contact_number) VALUES
-(1, 'Happy Paws', 1, '+91 98765 43210'),
-(2, 'Animal Haven', 98, '+91 87654 32109'),
-(3, 'Furry Friends', 15, '+91 76543 21098'),
-(4, 'Paw Paradise', 77, '+91 65432 10987'),
-(5, 'Rescue Ranch', 75, '+91 54321 09876'),
-(6, 'Cuddly Corner', 5, '+91 43210 98765'),
-(7, 'Kindred Spirits', 96, '+91 32109 87654'),
-(8, 'Heavenly Creatures', 83, '+91 21098 76543'),
-(9, 'Paws & Claws', 95, '+91 10987 65432'),
-(10, 'Wagging Tails', 10, '+91 09876 54321'),
-(11, 'Four-legged Palace', 80, '+91 12345 67890'),
-(12, 'Furever Home', 91, '+91 23456 78901'),
-(13, 'Critter Camp', 89, '+91 34567 89012'),
-(14, 'Animal Kingdom', 74, '+91 45678 90123'),
-(15, 'Barkville', 92, '+91 56789 01234'),
-(16, 'Majestic Tails', 100, '+91 67890 12345'),
-(17, 'Feathers & Fur', 84, '+91 78901 23456'),
-(18, 'Meow Mansion', 71, '+91 89012 34567'),
-(19, 'Woof Wonderland', 93, '+91 90123 45678'),
-(20, 'Hearty Homes', 85, '+91 91234 56789');
 
 
 -- creating table to store list of animals in all the adoption center
@@ -713,6 +686,38 @@ INSERT INTO ANIMALS_LIST_IN_ADOPTION_CENTERS (animal_id, center_id, animal_count
 (42, 18, 14), (43, 18, 13), (44, 18, 12), (45, 18, 11), (46, 18, 10), (47, 18, 9), (48, 18, 8), (49, 18, 7), (50, 18, 6), (51, 18, 5),
 (52, 19, 16), (53, 19, 18), (54, 19, 7), (55, 19, 9), (56, 19, 13), (57, 19, 10), (58, 19, 11), (59, 19, 6), (60, 19, 12), (18, 19, 20),
 (19, 20, 5), (24, 20, 17), (25, 20, 16), (26, 20, 15), (28, 20, 14), (29, 20, 13), (41, 20, 12), (42, 20, 11), (43, 20, 10), (44, 20, 9);
+
+
+DELIMITER //
+CREATE PROCEDURE AddCustomerAndCredentials(
+    IN p_name VARCHAR(50),
+    IN p_contact VARCHAR(15),
+    IN p_email VARCHAR(50),
+    IN p_password VARCHAR(50)
+)
+BEGIN
+    DECLARE v_customer_id INT;
+    DECLARE v_total_count INT;
+    DECLARE v_address_id INT DEFAULT 84;
+    DECLARE v_sponsor_amount INT DEFAULT 0;
+    DECLARE v_sponsor boolean DEFAULT false; 
+
+    -- Calculate the next customer_id based on total count + 1
+    SELECT COUNT(*) INTO v_total_count FROM CUSTOMER;
+    SET v_customer_id = v_total_count + 1;
+
+    -- Insert into CUSTOMER table
+    INSERT INTO CUSTOMER(customer_id, name, email, address_id, contact_number, sponsor, sponsor_amount)
+    VALUES(v_customer_id, p_name, p_email, v_address_id, p_contact, v_sponsor, v_sponsor_amount);
+
+    -- Insert into login_credentials table
+    INSERT INTO login_credentials(email, password)
+    VALUES(p_email, p_password);
+END;
+//
+DELIMITER ;
+
+CALL AddCustomerAndCredentials('John Doe', '1234567890', 'john.doe@gmail.com', 'securepassword');
 
 
 
--- routes/authentication.js
+++ routes/authentication.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const router = express.Router();
-const bcrypt = require('bcrypt');
 const connection = require('../db');
 
 router.use(express.json()); 
@@ -30,6 +29,44 @@ router.post('/login', (req, res) => {
         }
 
         res.status(200).json({ status: 'Logged in successfully' });
+    });
+});
+
+
+router.post('/signUp', (req, res) => {
+    console.log(req.body)
+    const name = req.body.name
+    const email = req.body.email;
+    const password = req.body.password;
+    const contact = req.body.contact;
+
+    const query = "SELECT * FROM login_credentials WHERE email = \'"+email + "\'";
+    console.log(query);
+
+    connection.query(query,(err, results) => {
+        if (err) {
+            console.error('Error fetching data:', err);
+            return results;
+        }
+        console.log(results)
+        if (results.length !== 0) {
+            return res.status(401).json({ status: 'Email already exists' });
+        } else {
+            const count = "select count(*) from CUSTOMER;"
+            var presentCount = 0;
+            connection.query(count, (err,results) => {
+                if(err) {
+                    return results;
+                }
+                presentCount = results;
+            });
+            const addQuery = "INSERT INTO CUSTOMER (customer_id, name, email, address_id, contact_number, sponsor, sponsor_amount) VALUES ( " + presentCount + ", " + name + ", " + email + ", " + 84 + ", " + contact + ", " + false + ", "+ 0 +" ;";
+            
+            res.status(200).json({ status: 'Account created successfully' });
+        }
+
+
+        
     });
 });
 
